6b7c88c1a4c4bc2d33b828440f079ebaba1522c2
ArielleM1 <100953317+ArielleM1@users.noreply.github.com>
2022-07-14 16:42:43 +0300

    Добавила файлы регистрации
    Почему-то форма ругается...

---
+++ reg_obr.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+$mysqli = mysqli_connect("localhost", "uksijkgz_0540", "0540", "uksijkgz_0540");
+if ($mysqli == false) {
+  print("error");
+} else {
+  // print("Соединение установлено успешно");
+
+  $name = $_POST['name'];
+  $lastname = $_POST['lastname'];
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
+
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' AND `pass`='$pass'");
+
+  if ($result->num_rows != 0) {
+    print("exist");
+  } else {
+    $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+    print("ok");
+  }
+}
+
+
+
+// echo "<hr>";
+// echo "Имя: $name<br>
+// Фамилия: $lastname<br>
+// E-mail: $email<br>
+// Пароль: $pass";
