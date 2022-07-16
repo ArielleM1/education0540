c9e55578a5e8ea77e11d07418dda37328ba8fb3c
ArielleM1 <100953317+ArielleM1@users.noreply.github.com>
2022-07-16 09:49:19 +0300

    Убрала лишнее
    Удалила закомментированные строки, и удалила пробел после email

--- reg_obr.php
+++ reg_obr.php
@@ -5,14 +5,13 @@ $mysqli = mysqli_connect("localhost", "uksijkgz_0540", "0540", "uksijkgz_0540");
 if ($mysqli == false) {
   print("error");
 } else {
-  // print("Соединение установлено успешно");
 
   $name = $_POST['name'];
   $lastname = $_POST['lastname'];
   $email = $_POST['email'];
   $pass = $_POST['pass'];
 
-  $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' ");
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'");
 
   if ($result->num_rows != 0) {
     print("exist");
@@ -21,11 +20,3 @@ if ($mysqli == false) {
     print("ok");
   }
 }
-
-
-
-// echo "<hr>";
-// echo "Имя: $name<br>
-// Фамилия: $lastname<br>
-// E-mail: $email<br>
-// Пароль: $pass";
